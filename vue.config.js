f546c0588dc0b1e65181528fe8726a9a5c459c6c
WilfredShen <54000440+WilfredShen@users.noreply.github.com>
2020-11-13 22:53:44 +0800

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  publicPath: "./",
+  outputDir: "dist",
+  productionSourceMap: false,
+  devServer: {
+    host: "localhost",
+    port: "9090",
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": "",
+        },
+      },
+    },
+  },
+}
