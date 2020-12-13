f73c07aa126a72cb3229ad01dd177b371e2d668c
ZouXyouYi <72750250+ZouXyouYi@users.noreply.github.com>
2020-12-13 17:55:41 +0800

    上传、更改评估结果

--- src/components/manager/changeresult.vue
+++ src/components/manager/changeresult.vue
@@ -14,18 +14,20 @@
       <el-divider></el-divider>
       <el-form ref="ruleForm" label-width="80px">
         <el-form-item label="专业查询">
-          <el-input v-model="searchmajor" size="small" style="width: 30%" placeholder="模糊查询"></el-input>
+          <el-input v-model="searchmajor" size="small" style="width: 30%" placeholder="模糊查询" clearable></el-input>
+          <span style="margin-left: 10px;color: dimgray">(多个专业查询，以空格相隔)</span>
         </el-form-item>
         <el-form-item label="学校查询">
-          <el-input v-model="searchschool" size="small" style="width: 30%" placeholder="模糊查询"></el-input>
+          <el-input v-model="searchschool" size="small" style="width: 30%" placeholder="模糊查询" clearable></el-input>
           <el-button type="primary" style="float: right;margin:0 20px 10px 10px" @click="getResult">查询</el-button>
+          <span style="margin-left: 10px;color: dimgray">(多个学校模糊查询关键字，以空格相隔，查找同时存在这些关键字的学校)</span>
         </el-form-item>
 
       </el-form>
       <div>
         <el-table :data="tabledata" :header-cell-style="{background:'#eef1f6',color:'#606266'}" border stripe>
           <el-table-column prop="mid" label="专业代码" align="center"></el-table-column>
-          <el-table-column prop="mname" label="学科名称" align="center"></el-table-column>
+          <el-table-column prop="mname" label="专业名称" align="center"></el-table-column>
           <el-table-column prop="cid" label="高校代码" align="center"></el-table-column>
           <el-table-column prop="cname" label="高校名称" align="center"></el-table-column>
           <el-table-column prop="result" label="评估结果" align="center"></el-table-column>
@@ -34,8 +36,7 @@
             label="操作"
             width="100px">
             <template slot-scope="scope">
-              <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
-              <el-button type="text" size="small">编辑</el-button>
+              <el-button type="text" size="small" @click="handleClick(scope.$index,scope.row)">编辑</el-button>
             </template>
           </el-table-column>
         </el-table>
@@ -45,12 +46,36 @@
         确认修改
       </el-button>
     </div>
+    <el-dialog title="修改评估结果" :visible.sync="dialogFormVisible">
+      <el-form>
+        <el-form-item label="专业代码">
+          <el-input v-model="row.mid"></el-input>
+        </el-form-item>
+        <el-form-item label="专业名称">
+          <el-input v-model="row.mname"></el-input>
+        </el-form-item>
+        <el-form-item label="学校代码">
+          <el-input v-model="row.cid"></el-input>
+        </el-form-item>
+        <el-form-item label="学校名称">
+          <el-input v-model="row.cname"></el-input>
+        </el-form-item>
+        <el-form-item label="评估结果">
+          <el-input v-model="row.result"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="confirmUpdate">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
   import {getMajor, getSchool} from "../../assets/lib/getHomeServe";
   import axios from "axios";
+  import {updateEvaluation} from "../../assets/lib/setManager";
 
   export default {
     data() {
@@ -64,111 +89,118 @@
         major: [],
         schoolmap: {},
         majormap: {},
+        dialogFormVisible: false,
+        row: [],
       }
     },
     methods: {
       getResult() {
-        if (this.searchmajor === '' && this.searchschool === '') {//没有进行任何模糊查询
-          this.$confirm(`没有进行模糊查询，确定查询所有的学校专业吗？`, '提示')
+        this.tabledata = [];
+        if (this.round === '') {
+          this.$confirm(`请选择轮次！`, '提示')
             .then((res) => {
               console.log(res);
-              if (this.round === '') {
-                this.$confirm(`请选择轮次！`, '提示')
-                  .then((res) => {
-                    console.log(res);
-                  })
-                  .catch((err) => {
-                    console.log(err);
-                  })
-              }
-              this.tabledata = this.evaluation;
-              console.log("情况1table", this.tabledata);
             })
             .catch((err) => {
               console.log(err);
             })
-        } else {//进行了模糊查询
-          let list = [];
-          if (this.searchmajor !== '' && this.searchschool !== '') {
-            let fsmajor = this.searchmajor.trim().split(/\s+/);
-            for (let j = 0; j < this.major.length; j++) {//匹配专业模糊查询
-              let temp1 = this.major[j].mname;
-              let temp2 = this.major[j].mid;
-              for (let i = 0; i < fsmajor.length; i++) {
-                if (temp1.match(fsmajor[i]) != null || temp2.match(fsmajor[i]) != null) {
-                  list = list.concat(this.evaluation.filter(item => item.mid === temp2));
-                  break;
-                }
-              }
-            }
-            let list2 = [];
-            let temp1 = '', temp2 = '';
-            let fschool = this.searchschool.trim().split(/\s+/);
-            console.log(list);
-            for (let j = 0; j < list.length; j++) {
-              temp1 = this.list[j].cid;
-              console.log(temp1);
-              temp2 = this.schoolmap[temp1].cname;
-              for (let i = 0; i < fschool.length; i++) {
-                if (temp1.match(fschool[i]) != null || temp2.match(fschool[i]) != null)
-                  console.log(1, temp2, fschool[i]);
-                list2 = list2.concat(list.filter(item => item.cid === temp1));
-              }
-            }
-            this.tabledata = list2;
-            console.log("情况2table", list2);
-          } else if (this.searchmajor !== '') {
-            let fsmajor = this.searchmajor.trim().split(/\s+/);
-            console.log("fsm", fsmajor);
-            for (let j = 0; j < this.major.length; j++) {//匹配专业模糊查询
-              let temp1 = this.major[j].mname;
-              let temp2 = this.major[j].mid;
-              for (let i = 0; i < fsmajor.length; i++) {
-                if (temp1.match(fsmajor[i]) != null || temp2.match(fsmajor[i]) != null) {
-                  console.log(1, temp1);
-                  list = list.concat(this.evaluation.filter(item => item.mid === temp2));
-                  break;
-                }
-              }
-            }
-            this.tabledata = list;
-            console.log("情况3table", this.tabledata);
-          } else if (this.searchschool !== '') {//进行学校模糊查询
-            let fschool = this.searchschool.trim().split(/\s+/);
-            for (let j = 0; j < this.school.length; j++) {
-              let temp1 = this.school[j].cid;
-              let temp2 = this.school[j].cname;
-              for (let i = 0; i < fschool.length; i++) {
-                if (temp1.match(fschool[i]) != null || temp2.match(fschool[i]) != null)
-                  list = list.concat(this.evaluation.filter(item => item.cid === temp1));
-              }
-            }
-            this.tabledata = list;
-            console.log("情况4table", this.tabledata);
-          }
-          if (this.round === '') {
-            this.$confirm(`请选择轮次！`, '提示')
+        } else {
+          if (this.searchmajor === '' && this.searchschool === '') {//没有进行任何模糊查询
+            this.$confirm(`没有进行模糊查询，确定查询所有的学校专业吗？`, '提示')
               .then((res) => {
                 console.log(res);
+                this.tabledata = this.evaluation;
+                console.log("情况1table", this.tabledata);
+                this.tabledata.forEach(item => {
+                  this.$set(item, 'cname', "");
+                  this.$set(item, 'mname', "");
+                });
+                for (let i = 0; i < this.tabledata.length; i++) {
+                  this.tabledata[i].cname = this.schoolmap[this.tabledata[i].cid].cname;
+                  this.tabledata[i].mname = this.majormap[this.tabledata[i].mid].mname;
+                }
               })
-              .catch((err) => {
-                console.log(err);
-              })
+          } else {//进行了模糊查询
+            let list = [];
+            if (this.searchmajor !== '' && this.searchschool !== '') {//两个模糊查询都进行了
+              let fsmajor = this.searchmajor.trim().split(/\s+/);
+              for (let j = 0; j < this.major.length; j++) {//匹配专业模糊查询
+                let temp1 = this.major[j].mname;
+                let temp2 = this.major[j].mid;
+                for (let i = 0; i < fsmajor.length; i++) {
+                  if (temp1.match(fsmajor[i]) != null || temp2.match(fsmajor[i]) != null) {
+                    list = list.concat(this.evaluation.filter(item => item.mid === temp2));
+                    break;
+                  }
+                }
+              }
+              console.log("lll", list)
+              let fschool = this.searchschool.trim().split(/\s+/);
+              let flag = 1;
+              for (let j = 0; j < list.length; j++) {
+                let temp1 = list[j].cid;
+                let temp2 = this.schoolmap[list[j].cid].cname;
+                flag = 1;
+                for (let i = 0; i < fschool.length; i++) {
+                  if (temp1.match(fschool[i]) == null && temp2.match(fschool[i]) == null) {
+                    flag = 0;
+                    break;
+                  }
+                }
+                if (flag) {
+                  this.tabledata.push(list[j]);
+                }
+              }
+              console.log("情况2table", this.tabledata);
+            } else if (this.searchmajor !== '') {//进行专业模糊查询
+              let fsmajor = this.searchmajor.trim().split(/\s+/);
+              console.log("fsm", fsmajor);
+              for (let j = 0; j < this.major.length; j++) {//匹配专业模糊查询
+                let temp1 = this.major[j].mname;
+                let temp2 = this.major[j].mid;
+                for (let i = 0; i < fsmajor.length; i++) {
+                  if (temp1.match(fsmajor[i]) != null || temp2.match(fsmajor[i]) != null) {
+                    console.log(1, temp1);
+                    list = list.concat(this.evaluation.filter(item => item.mid === temp2));
+                    break;
+                  }
+                }
+              }
+              this.tabledata = list;
+              console.log("情况3table", this.tabledata);
+            } else if (this.searchschool !== '') {//进行学校模糊查询
+              let fschool = this.searchschool.trim().split(/\s+/);
+              let flag = 1;
+              for (let j = 0; j < this.evaluation.length; j++) {
+                let temp1 = this.evaluation[j].cid;
+                let temp2 = this.schoolmap[this.evaluation[j].cid].cname;
+                flag = 1;
+                for (let i = 0; i < fschool.length; i++) {
+                  if (temp1.match(fschool[i]) == null && temp2.match(fschool[i]) == null) {
+                    flag = 0;
+                    break;
+                  }
+                }
+                if (flag) {
+                  this.tabledata.push(this.evaluation[j]);
+                }
+              }
+              console.log("情况4table", this.tabledata);
+            }
+            if (this.tabledata.length === 0) {
+              this.$message('该查询无结果！');
+            } else { //添加学校名、专业名字段
+              this.tabledata.forEach(item => {
+                this.$set(item, 'cname', "");
+                this.$set(item, 'mname', "");
+              });
+              for (let i = 0; i < this.tabledata.length; i++) {
+                this.tabledata[i].cname = this.schoolmap[this.tabledata[i].cid].cname;
+                this.tabledata[i].mname = this.majormap[this.tabledata[i].mid].mname;
+              }
+            }
           }
-          // list=list.filter(item => item.round=this.round);
-          this.tabledata = list;
-          console.log("table", this.tabledata);
         }
-        //添加学校名、专业名字段
-        this.tabledata.forEach(item => {
-          this.$set(item, 'cname', "");
-          this.$set(item, 'mname', "");
-        });
-        for (let i = 0; i < this.tabledata.length; i++) {
-          this.tabledata[i].cname = this.schoolmap[this.tabledata[i].cid].cname;
-          this.tabledata[i].mname = this.majormap[this.tabledata[i].mid].mname;
-        }
-        console.log("all table", this.tabledata);
       },
       //查询评估结果
       getEvaluation() {
@@ -183,6 +215,32 @@
           })
           .catch((err) => {
             console.log(err)
+          })
+      },
+      //编辑
+      handleClick(index, row) {
+        this.row = row;
+        this.tabledata[index] = row;
+        this.dialogFormVisible = true;
+      },
+      confirmUpdate() {
+        this.dialogFormVisible = false;
+        updateEvaluation(this.row.cid, this.row.mid, this.row.result, this.round)
+          .then((res) => {
+            console.log(res);
+            this.$message({
+              showClose: true,
+              message: '修改成功！',
+              type: 'success'
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+            this.$message({
+              showClose: true,
+              message: '修改失败！',
+              type: 'error'
+            });
           })
       }
     },
@@ -211,11 +269,9 @@
         .catch((err) => {
           console.log(err)
         });
-
     }
   }
 </script>
 
 <style scoped>
-
 </style>
