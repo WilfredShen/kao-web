8c1f6b98804636d7e1d31294d5e1a76abf314d38
hnuhzy <hnuhzy@126.com>
2020-12-14 00:25:57 +0800

    基本完成新闻上传和修改 合并学校详情修改后界面

--- src/components/manager/newslist.vue
+++ src/components/manager/newslist.vue
@@ -1,40 +1,97 @@
 <template>
   <div style="padding: 40px 10px 10px 10px">
     <div style="border: 1px solid darkgrey;padding: 20px 20px 40px 20px;width: 60%;height: 550px">
-      <el-table :data="tabledata" :header-cell-style="{background:'#eef1f6',color:'#606266'}" border stripe>
-        <el-table-column prop="up_time" label="上传时间" align="center"></el-table-column>
-        <el-table-column prop="admin_id" label="管理员ID" align="center"></el-table-column>
-        <el-table-column prop="school_name" label="高校名称" align="center"></el-table-column>
-        <el-table-column prop="title" label="标题" align="center"></el-table-column>
-        <el-table-column prop="edit" label="编辑" align="center">
-          <template>
-            <el-button size="small">编辑</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
+      <el-scrollbar style="height: 500px">
+        <el-table :data="tableData" :header-cell-style="{background:'#eef1f6',color:'#606266'}" border stripe>
+          <el-table-column prop="up_time" label="上传时间" align="center"></el-table-column>
+          <el-table-column prop="admin_id" label="管理员ID" align="center"></el-table-column>
+          <el-table-column prop="school_name" label="高校名称" align="center"></el-table-column>
+          <el-table-column prop="title" label="标题" align="center"></el-table-column>
+          <el-table-column prop="edit" label="编辑" align="center">
+            <el-button size="small" @click="edit()">编辑</el-button>
+          </el-table-column>
+        </el-table>
+      </el-scrollbar>
+      <el-dialog :visible.sync="editNews" :width="width">
+        <newsupdate @eIfCommit="ifEdit($event)"></newsupdate>
+      </el-dialog>
     </div>
   </div>
 </template>
 
 <script>
+
+  import {getCookie} from "../../assets/lib/utils";
+  import {getSchool} from "../../assets/lib/getHomeServe";
+  import newsupdate from "./newsupdate"
+
   export default {
+    props: {
+      width: {
+        type: String,
+        default: '34%'
+      }
+    },
+    components: {
+      newsupdate
+    },
     data() {
       return {
         hint: '当前轮次没有数据，请上传',
-        tabledata: [{
-          up_time: '2020-11-15',
-          admin_id: '01',
-          school_name: '湖南大学',
-          title: '校园网的爱恨情仇',
-          edit: ''
-        }, {
-          up_time: '2020-11-16',
-          admin_id: '01',
-          school_name: '北京大学',
-          title: '我与清华二三事',
-          edit: ''
-        }]
+        tableData: [],
+        editNews: false
       }
+    },
+    methods: {
+      ifEdit(data) {
+        this.editNews = data;
+      },
+      edit() {
+        this.editNews = true;
+      },
+      getSchMaps() {
+        if (JSON.stringify(this.$store.state.schoolMap) !== '{}') {
+          let sMap = this.$store.state.schoolMap;
+          for (const key in sMap) {
+            this.schoolIds.push(key);
+            this.schools.push(sMap[key])
+          }
+        } else {
+          getSchool().then((res) => {
+            res.data.forEach(row => {
+              this.schoolIds.push(row.cid);
+              this.schools.push(row.cname);
+              this.$store.commit("setSchMap", {
+                cname: row.cname,
+                cid: row.cid
+              });
+            })
+          }).catch((err) => {
+            console.log(err)
+          });
+        }
+      }
+    },
+    created() {
+      const uid = getCookie("uid");
+      this.$axios.get("/api/admin/q/news")
+        .then(res => {
+          console.log("获取新闻列表无误", res);
+          let items = res.data.data;
+          for (let i = 0; i < items.length; i++) {
+            this.tableData.push({
+              up_time: items[i].date,
+              admin_id: uid,
+              school_name: items[i].cid,
+              title: items[i].title
+            })
+          }
+        })
+        .catch(error => {
+          console.log("获取新闻列表有错误", error);
+        })
+
+
     }
   }
 </script>
