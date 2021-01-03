48dc94e042c83f10dc54be0f825dcd2aba2c0e92
Jessical7 <274235013@qq.com>
2021-01-03 15:53:16 +0800

    修改登录注册修改密码页面样式

--- src/components/forgot/index.vue
+++ src/components/forgot/index.vue
@@ -1,71 +1,69 @@
 <template>
-  <div id="forgot-wrapper">
-    <el-container>
-      <el-header>
-        <span style="text-align: left;font-weight: bold;font-size: 30px">修改密码</span>
-      </el-header>
-      <el-main>
-        <el-form
-          ref="forgotForm"
-          :model="forgotForm"
-          :rules="forgotRules"
-          label-position="left"
-          label-width="80px"
-        >
-          <el-form-item
-            class="myLogin"
-            label="用户名"
-            prop="username"
+  <body>
+  <div
+    class="container"
+    id="container"
+  >
+    <div id="forgot-wrapper">
+      <el-container>
+        <el-main style="width: 80%;margin-left: 20px;margin-top: 60px">
+          <el-form
+            ref="forgotForm"
+            :model="forgotForm"
+            :rules="forgotRules"
+            label-position="left"
+            label-width="80px"
           >
-            <el-input
-              v-model="forgotForm.username"
-              placeholder="请输入用户名"
-            ></el-input>
-          </el-form-item>
-          <el-form-item
-            class="myLogin"
-            label="新密码"
-            prop="password"
-          >
-            <el-input
-              v-model="forgotForm.password"
-              :type="status.showPassword ? '' : 'password'"
-              placeholder="请输入新密码"
+            <el-form-item
+              class="myLogin"
+              label="用户名"
+              prop="username"
             >
-              <i
-                slot="suffix"
-                :style="{ color: status.showPassword ? '#409EFF' : '' }"
-                class="el-icon-view"
-                @click="status.showPassword = !status.showPassword"
-              ></i>
-            </el-input>
-          </el-form-item>
-          <el-form-item
-            class="myLogin"
-            label="确认密码"
-            prop="confPass"
-          >
-            <el-input
-              v-model="forgotForm.confPass"
-              :type="status.showConfPass ? '' : 'password'"
-              placeholder="请再次输入密码"
+              <el-input
+                v-model="forgotForm.username"
+                placeholder="请输入用户名"
+              ></el-input>
+            </el-form-item>
+            <el-form-item
+              class="myLogin"
+              label="新密码"
+              prop="password"
             >
-              <i
-                slot="suffix"
-                :style="{ color: status.showConfPass ? '#409EFF' : '' }"
-                class="el-icon-view"
-                @click="status.showConfPass = !status.showConfPass"
-              ></i>
-            </el-input>
-          </el-form-item>
-          <el-form-item
-            class="myLogin"
-            label="手机号"
-            prop="phone"
-          >
-            <el-row
-              justify="space-between"
-              type="flex"
+              <el-input
+                v-model="forgotForm.password"
+                :type="status.showPassword ? '' : 'password'"
+                placeholder="请输入新密码"
+              >
+                <i
+                  slot="suffix"
+                  :style="{ color: status.showPassword ? '#409EFF' : '' }"
+                  class="el-icon-view"
+                  @click="status.showPassword = !status.showPassword"
+                ></i>
+              </el-input>
+            </el-form-item>
+            <el-form-item
+              class="myLogin"
+              label="确认密码"
+              prop="confPass"
+            >
+              <el-input
+                v-model="forgotForm.confPass"
+                :type="status.showConfPass ? '' : 'password'"
+                placeholder="请再次输入密码"
+              >
+                <i
+                  slot="suffix"
+                  :style="{ color: status.showConfPass ? '#409EFF' : '' }"
+                  class="el-icon-view"
+                  @click="status.showConfPass = !status.showConfPass"
+                ></i>
+              </el-input>
+            </el-form-item>
+            <el-form-item
+              class="myLogin"
+              label="手机号"
+              prop="phone"
             >
               <el-col :span="14">
                 <el-input
@@ -75,7 +73,7 @@
               </el-col>
               <el-col :span="9">
                 <el-button
-                  style="width: 100%; padding-left: 0; padding-right: 0; text-align: center"
+                  style="width: 100%; padding-left: 0; padding-right: 0; margin-left:8px;text-align: center"
                   @click="getCode()"
                 >
                   <span v-if="status.resend > 0">{{ status.resend }} 秒后</span>
@@ -83,41 +81,53 @@
                   <span v-else>重新获取</span>
                 </el-button>
               </el-col>
-            </el-row>
-          </el-form-item>
-          <el-form-item
-            class="myLogin"
-            label="验证码"
-            prop="code"
-          >
-            <el-input
-              v-model="forgotForm.code"
-              placeholder="请输入验证码"
-            ></el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-button
-              style="width: 100%"
-              type="primary"
-              @click="submit()"
+            </el-form-item>
+            <el-form-item
+              show-message
+              class="myLogin"
+              label="验证码"
+              prop="code"
             >
-              <span>提交</span>
-            </el-button>
-          </el-form-item>
-        </el-form>
-      </el-main>
-      <el-footer>
-        <div style="text-align: right">
-          <router-link
-            :to="{ path: '/login'}"
-            style="text-decoration: underline; cursor: pointer"
-          >
-            前往登录
-          </router-link>
-        </div>
-      </el-footer>
-    </el-container>
+              <el-input
+                v-model="forgotForm.code"
+                placeholder="请输入验证码"
+              ></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button
+                style="width: 100%;background-color: #1e56a0"
+                type="primary"
+                @click="submit()"
+              >
+                <span>提交</span>
+              </el-button>
+            </el-form-item>
+          </el-form>
+        </el-main>
+        <el-footer style="margin-bottom: 10px">
+          <div style="text-align: right">
+            <router-link
+              class="link"
+              :to="{ path: '/login'}"
+              style="cursor: pointer"
+            >
+              前往登录
+            </router-link>
+          </div>
+        </el-footer>
+      </el-container>
+    </div>
+    <div class="overlay-container">
+      <div class="overlay">
+        <h1>Change Password!</h1>
+        <br>
+        <p>
+          Silly goose,To keep connected with us please change your password!
+        </p>
+      </div>
+    </div>
   </div>
+  </body>
 </template>
 
 <script>
@@ -170,7 +180,7 @@
       };
     },
     methods: {
-      getCode: function() {
+      getCode: function () {
         if (this.forgotForm.phone == null || this.forgotForm.phone === "") {
           this.$alert('请输入手机号！', '提示', {
             confirmButtonText: '确定',
@@ -210,7 +220,7 @@
             console.log(err.data);
           });
       },
-      submit: function() {
+      submit: function () {
         this.$axios
           .post("/api/visitor/update-password", {
             username: this.forgotForm.username,
@@ -224,13 +234,11 @@
               setTimeout(() => {
                 this.$router.push("/login");
               }, 3000);
-            } else if(res.data.status===3040){
+            } else if (res.data.status === 3040) {
               this.$message.error("修改失败！");
-            }
-            else if(res.data.status===403){
+            } else if (res.data.status === 403) {
               this.$message.error("该用户不存在！");
-            }
-            else if(res.data.status===422){
+            } else if (res.data.status === 422) {
               this.$message.error("手机号与该用户不匹配")
             }
           })
@@ -244,9 +252,76 @@
 
 <style scoped>
   #forgot-wrapper {
-    width: 25%;
-    margin: auto;
-    min-width: 450px;
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+    height: 100%;
+    width: 50%;
+    padding: 0 35px;
+    transform: translateX(-5%);
+  }
+
+  body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #F6F5F7;
+  }
+
+  .container {
+    position: relative;
+    width: 900px;
+    max-width: 100%;
+    min-height: 480px;
+    background-color: white;
+    box-shadow: 0 14px 28px rgba(0, 0, 0, .25), 0 10px 10px rgba(0, 0, 0, .22);
+    border-radius: 10px;
+    overflow: hidden;
+  }
+
+  .link {
+    height: 20px;
+    color: #9AA9B7;
+    text-decoration: none;
+    margin: 5px 0 15px;
+    font-size: 15px;
+    text-transform: capitalize;
+  }
+
+  h1 {
+    text-transform: capitalize;
+    font-size: 35px;
+    text-align: center;
+  }
+
+  p {
+    margin: 10px 0 30px;
+  }
+
+  .overlay-container {
+    position: absolute;
+    right: 0;
+    width: 50%;
+    height: 100%;
+    background-color: #163172;
+    overflow: hidden;
+    transform: translateX(0%);
+  }
+
+  .overlay {
+    position: absolute;
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0 40px;
+    text-align: center;
+    color: white;
+    font-size: 15px;
   }
 
   .el-icon-view {
